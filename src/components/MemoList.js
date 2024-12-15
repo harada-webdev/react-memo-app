fbbbbc22c42891cfebf94aa67ce951db027d285a
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2024-12-15 22:21:30 +0900

    Add CSS

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,11 +1,28 @@
+import React, { useState } from "react";
+import "../styles/MemoList.css";
+
 export default function MemoList({ memos, setEditingMemo }) {
+  const [selectedMemo, setSelectedMemo] = useState(null);
   return (
-    <ol>
-      {memos.map((memo) => (
-        <li key={memo.id} onClick={() => setEditingMemo(memo)}>
-          {memo.content.split("\n")[0] || "無題"}
-        </li>
-      ))}
-    </ol>
+    <div className="memo-list">
+      <ol>
+        {memos.map((memo) => (
+          <li
+            key={memo.id}
+            onClick={() => {
+              setEditingMemo(memo);
+              setSelectedMemo(memo);
+            }}
+            className={
+              selectedMemo && selectedMemo.id === memo.id ? "selected" : ""
+            }
+          >
+            <div className="memo-title">
+              {memo.content.split("\n")[0] || "無題"}
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
